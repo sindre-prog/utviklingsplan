19b32d327d8ad43e826c157ef872478785fd2124
Sindre Ræder <sindre@raederog.com>
2026-05-25 19:35:15 +0200

    Clean up portal foundation styling

--- js/resources/resources.api.js
+++ js/resources/resources.api.js
@@ -1,8 +1,8 @@
-export * from "./resources.constants.js?v=polish-61";
-export * from "./resources.queries.js?v=polish-61";
-export * from "./resources.mutations.js?v=polish-61";
-export * from "./resources.renderer.js?v=polish-61";
-export * from "./resources.components.js?v=polish-61";
-export * from "./resources.seed.js?v=polish-61";
+export * from "./resources.constants.js?v=polish-62";
+export * from "./resources.queries.js?v=polish-62";
+export * from "./resources.mutations.js?v=polish-62";
+export * from "./resources.renderer.js?v=polish-62";
+export * from "./resources.components.js?v=polish-62";
+export * from "./resources.seed.js?v=polish-62";
 
-export const RESOURCE_LIBRARY_MODULE_VERSION = "polish-61";
+export const RESOURCE_LIBRARY_MODULE_VERSION = "polish-62";
--- js/resources/resources.renderer.js
+++ js/resources/resources.renderer.js
@@ -1,4 +1,4 @@
-import { RESOURCE_BLOCK_TYPES } from "./resources.constants.js?v=polish-61";
+import { RESOURCE_BLOCK_TYPES } from "./resources.constants.js?v=polish-62";
 
 function assertElementFactory(createElement) {
   if (typeof createElement !== "function") {
