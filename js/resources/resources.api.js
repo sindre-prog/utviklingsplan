ee69b8dbff077a444e086ffd19343cc7c5afeaf5
Sindre Ræder <sindre@raederog.com>
2026-05-25 18:21:20 +0200

    Refine portal color logic and Retning copy

--- js/resources/resources.api.js
+++ js/resources/resources.api.js
@@ -1,8 +1,8 @@
-export * from "./resources.constants.js?v=polish-59";
-export * from "./resources.queries.js?v=polish-59";
-export * from "./resources.mutations.js?v=polish-59";
-export * from "./resources.renderer.js?v=polish-59";
-export * from "./resources.components.js?v=polish-59";
-export * from "./resources.seed.js?v=polish-59";
+export * from "./resources.constants.js?v=polish-60";
+export * from "./resources.queries.js?v=polish-60";
+export * from "./resources.mutations.js?v=polish-60";
+export * from "./resources.renderer.js?v=polish-60";
+export * from "./resources.components.js?v=polish-60";
+export * from "./resources.seed.js?v=polish-60";
 
-export const RESOURCE_LIBRARY_MODULE_VERSION = "polish-59";
+export const RESOURCE_LIBRARY_MODULE_VERSION = "polish-60";
